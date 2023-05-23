9a29e571b7a1a9f289649344569df7eb9de65907
beatrizcastr0 <beatrizcastroo30@gmail.com>
2023-05-23 15:06:04 -0300

    Meu primeiro commit

---
+++ result.py
@@ -0,0 +1,6 @@
+a = (input("Digite o primeiro número")).strip
+b = (input("Digite o segundo número")).strip
+c = (input("Digite o terceiro número")).strip
+#essa função irá calcular os 3 números inseridos pelo usuário
+def calcular (a,b,c):
+    return 2* a + 5 * b - c
---
+++ resultadofuncao.py
@@ -0,0 +1,8 @@
+a = (input(""))
+b = (input(""))
+c = (input(""))
+
+def calcular (a,b,c):
+    return 2* a + 5 * b -c 
+
+print("O resultado final da função é:" def)
